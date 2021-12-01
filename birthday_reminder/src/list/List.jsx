95db681a4b4a013637a681d52c11e57122d181d1
joyadeep <joyadeep.limbu@gmail.com>
2021-12-01 14:45:39 +0545

    after fixing warning

--- birthday_reminder/src/list/List.jsx
+++ birthday_reminder/src/list/List.jsx
@@ -8,7 +8,7 @@ export const List = ({data}) => {
                           data.map((item,index)=>{
                               return(
                                   <div className="item" key={index}>
-                                      <div className="image"> <img src={item.image} alt="user" style={item.gender=="M"?{border:"4px solid rgb(18, 142, 214)"}:{border:"4px solid pink"}} ></img> </div>
+                                      <div className="image"> <img src={item.image} alt="user" style={item.gender==="M"?{border:"4px solid rgb(18, 142, 214)"}:{border:"4px solid pink"}} ></img> </div>
                                       <div className="info">
                                           <div className="name">{item.name}</div>
                                           <div className="age">{item.age} years</div>
